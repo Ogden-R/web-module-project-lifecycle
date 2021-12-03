cdde022f4c8a6d58a4bc6baa0429c133441ba3fa
Ogden-R <rebeccaogden97@gmail.com>
2021-12-03 13:47:44 -0600

    finished with project now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import User from './components/User';
+import axios from 'axios';
+import FollowerList from './components/FollowerList';
 
 class App extends React.Component {
+  state={
+    currentUser: "Ogden-R",
+    user: {},
+    followers: []
+  }
+
+  componentDidMount(){
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then(resp=> {
+      this.setState({
+        ...this.state,
+        user: resp.data
+      });
+    })
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if(this.state.user !== prevState.user){
+        axios.get(`https://api.github.com/users/${this.state.currentUser}/followers`)
+        .then(resp=> {
+        this.setState({
+          ...this.state,
+          followers: resp.data
+        });
+      })
+    }
+  }
+
+handleChange=(e)=> {
+  this.setState({
+    ...this.state,
+    currentUser: e.target.value
+  });
+}
+
+handleSubmit = (e) => {
+  e.preventDefault();
+  axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+  .then(resp=> {
+    this.setState({
+      ...this.state,
+      user: resp.data
+    });
+  });
+}
   render() {
+    console.log(this.state);
     return(<div>
-      Github Card
+      <h1>GitHub Info</h1>
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="Github Handle" 
+        onChange={this.handleChange}/>
+        <button>Search</button>
+      </form>
+      <User user={this.state.user}/>
+      <FollowerList followers={this.state.followers}/>
     </div>);
   }
 }
